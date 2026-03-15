ffc716dbc6f4b5815290974896906c42354702ed
Shivax4 <166213039+Shivax4@users.noreply.github.com>
2026-03-15 20:38:05 +0530

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,8 @@
+questions = [
+"What is the primary reason for leaving the organization?",
+"How would you describe your overall experience with the company?",
+"What did you like most about working here?",
+"What could the company improve?",
+"How was your relationship with your manager and team?",
+"Would you recommend this company to others? Why or why not?"
+]
